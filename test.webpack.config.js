500026f4fb37cb2acca01c757feecaa8809af987
Ilfat Galiev <ilfat.galiev@akvelon.com>
2018-08-06 14:07:12 +0300

    Update webpack conf

--- test.webpack.config.js
+++ test.webpack.config.js
@@ -13,7 +13,24 @@ module.exports = {
             {
                 test: /\.json$/,
                 loader: 'json-loader'
-              }
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            paths: [path.resolve(__dirname, 'node_modules')]
+                        }
+                    }
+                ]
+            }
         ]
     },
     externals: {
