63b76949dc5cb8af43fad43e1fd30fa1cebe64b1
REDMOND\v-bdjalalov <boburtjalalov@gmail.com>
2025-10-13 19:26:02 +0500

    updated pbi-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,7 @@
 import powerbiVisualsConfigs from "eslint-plugin-powerbi-visuals";
-import path from 'path';
-
-// TEMP SOLUTION: Setting tsconfigRootDir for parserOptions to fix ESLint config resolution.
-// TODO: Update this approach when upgrading to newer versions of eslint-plugin-powerbi-visuals or ESLint.
-const recommended = {
-    ...powerbiVisualsConfigs.configs.recommended,
-    languageOptions: {
-        ...powerbiVisualsConfigs.configs.recommended.languageOptions,
-        parserOptions: {
-            ...powerbiVisualsConfigs.configs.recommended.languageOptions?.parserOptions,
-            tsconfigRootDir: path.resolve(),
-        },
-    },
-};
 
 export default [
-    recommended,
+    powerbiVisualsConfigs.configs.recommended,
     {
         ignores: [
             "node_modules/**",
